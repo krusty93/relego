03ba64fbffec741529633d1074df9ccb853549a9
Andrea Grillo <andrea.grillo@outlook.com>
2026-06-03 23:26:35 +0200

    Change site web URL to new domain (#313)
    Update the site URL in the configuration to reflect the new domain.
    
    Close #312

--- src/landing/astro.config.mjs
+++ src/landing/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://krusty93.github.io/relego',
+	site: 'https://relego.app/',
 	base: '/relego/',
 	srcDir: '.',
 });
